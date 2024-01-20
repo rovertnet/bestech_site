7922bb0f8b13591d13658e0aff080d44856accd5
Robert Matundu <matundukabamba@gmail.com>
2024-01-20 10:23:35 +0100

    update

--- src/composants/header/Scrollprogress.jsx
+++ src/composants/header/Scrollprogress.jsx
@@ -4,21 +4,20 @@ function Scrollprogress() {
   const { scrollYProgress } = useScroll()
   return (
     <>
-        <motion.div style={{
-          scaleX : scrollYProgress,
-          position: 'fixed',
-          top:0,
-          left:0,
-          right:0,
-          height:10,
-          transformOrigin:'0%',
-        }} 
-        className=" bg-blue-800 "
-      >
-
-        </motion.div>
+      <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 10,
+          transformOrigin: "0%",
+        }}
+        className=" bg-gradient-to-r from-blue-600 via-yellow-500 to-red-700 ... "
+      ></motion.div>
     </>
-  )
+  );
 }
 
 export default Scrollprogress
